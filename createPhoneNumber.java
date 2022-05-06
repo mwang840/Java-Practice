bcc6408665bad4885266c98ee9e04f68e5712c02
Maxwell Wang <maxwang@udel.edu>
2022-05-06 13:59:57 -0400

    reformatted functions to fix linter errors

---
+++ createPhoneNumber.java
@@ -0,0 +1,8 @@
+public class createPhoneNumber{
+  public static String phoneNum(int[] numbers) {
+    //Shorter version
+    return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+}
+}
+
+
