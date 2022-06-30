30f3cc8d0b3d9da0c5b77c37a242d343acf85361
mwang840 <maxwang@udel.edu>
2022-06-30 18:03:52 -0400

    scramble

---
+++ src/Scrambiles.java
@@ -0,0 +1,15 @@
+package src;
+
+//There are different ways to solve this
+//I can loop through the strings and check each character but it will be too long
+
+public class Scrambiles{
+    public static boolean scramble(String one, String two){
+        
+        return false;
+    }
+
+    public static void main(String []args){
+        
+    }
+}
