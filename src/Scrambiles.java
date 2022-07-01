148520c724f5221df7a8f444ae4e8cae22a07d54
Maxwell Wang <maxwang@udel.edu>
2022-06-30 22:17:57 -0400

    scramble my eggs

--- src/Scrambiles.java
+++ src/Scrambiles.java
@@ -1,15 +1,26 @@
 package src;
+import java.util.HashMap;
 
 //There are different ways to solve this
 //I can loop through the strings and check each character but it will be too long
-
+//However I can use a search algo or a hashmap (faster runtime)
+//Iterate over 
 public class Scrambiles{
     public static boolean scramble(String one, String two){
-        
+        //Initial case if 2nd string is undefined
+        HashMap<String, String>words = new HashMap<String, String>();
+        char[] arr1 = new char[one.length()];
+        char[] arr2 = new char[two.length()];
+        for(int i = 0; i < one.length(); ++i){
+            arr1[i] = one.charAt(i);
+        }
+        for(int i = 0; i < two.length(); ++i){
+            arr2[i] = two.charAt(i);
+        }
         return false;
     }
 
     public static void main(String []args){
-        
+        System.out.print("Hello");
     }
 }
