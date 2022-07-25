12ba0895a09690397fc004b54b9552d233ed4770
Maxwell Wang <maxwang@udel.edu>
2022-07-24 21:21:51 -0400

    counting bits

---
+++ src/BitCounting.java
@@ -0,0 +1,10 @@
+package src;
+public class BitCounting {
+    public static int countBits(int n){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
