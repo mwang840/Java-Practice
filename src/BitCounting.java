70e150e206525f261af4af9c266b5883e69fddab
Maxwell Wang <maxwang@udel.edu>
2022-07-25 08:53:44 -0400

    i've counted the bits

--- src/BitCounting.java
+++ src/BitCounting.java
@@ -1,10 +1,17 @@
 package src;
 public class BitCounting {
     public static int countBits(int n){
-        return 0;
+        String toBinary = Integer.toBinaryString(n);
+        int one = 0;
+        for(int i = 0; i < toBinary.length(); ++i){
+            if(toBinary.charAt(i) == '1'){
+                one++;
+            }
+        }
+        return one;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(countBits(1234));
     }
 }
