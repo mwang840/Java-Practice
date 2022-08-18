11fe91de6929e3023b6da58ac104152abfb6cf5c
mwang840 <maxwang@udel.edu>
2022-08-17 22:10:30 -0400

    pascals

---
+++ src/PascalsTriangle.java
@@ -0,0 +1,11 @@
+package src;
+
+public class PascalsTriangle{
+    public static long[] generate(int level){
+        return new long[0];
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
